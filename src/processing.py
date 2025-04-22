b38e344210e31df50850a185eb900daf6261ebba
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-04-22 15:39:27 +0300

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,9 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     state соответствует указанному значению"""
 
     return [item for item in list_of_dicts if item.get("state") == state]
+
+
+def sort_by_date(list_of_dicts: list, descending: bool = True) -> list:
+    """Функция, которая возвращает новый список, отсортированный по дате"""
+
+    return sorted(list_of_dicts, key=lambda item: item["date"], reverse=descending)
