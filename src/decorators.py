a1879f4afac3c7e603819480c0f3b1c1fbb68267
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-05-11 11:59:40 +0300

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,23 @@
+import functools
+
+def log(filename=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            log_message = ""
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+                return result
+            except Exception as e:
+                log_message = (f"{func.__name__} error: {type(e).__name__}. "
+                               f"Inputs: {args}, {kwargs}")
+                raise
+            finally:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(log_message + "\n")
+                else:
+                    print(log_message)
+        return wrapper
+    return decorator
