eba972f14176a57a44411a01612c740fb573ed49
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-05-22 22:21:15 +0300

    add logs to masks

--- src/masks.py
+++ src/masks.py
@@ -1,44 +1,44 @@
-import os
 import logging
+import os
 from typing import Union
 
-# Создаём директорию logs, если её нет
+# Создание директории logs, если она ещё не существует
 os.makedirs("logs", exist_ok=True)
 
-# Настройка логирования
-log_file_path = os.path.join("logs", "masks.log")
-
-logging.basicConfig(
-    filename=log_file_path,
-    filemode="w",  # Перезапись при запуске
-    format="%(asctime)s %(name)s %(levelname)s: %(message)s",
-    level=logging.DEBUG
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)  # Уровень логирования не ниже DEBUG
+file_handler = logging.FileHandler("logs/masks.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter(
+    "%(asctime)s %(name)s %(levelname)s: %(message)s"
 )
+file_handler.setFormatter(file_formatter)  # Установка форматера
 
-logger = logging.getLogger(__name__)
+# Добавление handler к логеру
+if not logger.hasHandlers():
+    logger.addHandler(file_handler)
 
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
-    """Возвращает маску номера карты в формате XXXX XX** **** XXXX"""
+    """Маскирует номер карты в формате XXXX XX** **** XXXX"""
     card_number = str(card_number).replace(" ", "")
 
     if not card_number or not card_number.isdigit() or len(card_number) != 16:
-        logger.warning("Некорректный номер карты: %s", card_number)
+        logger.error(f"Некорректный номер карты: {card_number}")
         return "Вы ввели некорректный номер карты"
 
-    masked_card_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-    logger.info("Карта успешно замаскирована.")
-    return masked_card_number
+    masked = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    logger.debug(f"Маскированный номер карты: {masked}")
+    return masked
 
 
 def get_mask_account(account_number: Union[str, int]) -> str:
-    """Возвращает маску номера счёта в формате **XXXX"""
+    """Маскирует номер счёта в формате **XXXX"""
     account_number = str(account_number).replace(" ", "")
 
     if not account_number or not account_number.isdigit() or len(account_number) != 20:
-        logger.warning("Некорректный номер счёта: %s", account_number)
+        logger.error(f"Некорректный номер счёта: {account_number}")
         return "Вы ввели некорректный номер счета"
 
-    masked_account_number = f"**{account_number[-4:]}"
-    logger.info("Счёт успешно замаскирован.")
-    return masked_account_number
+    masked = f"**{account_number[-4:]}"
+    logger.debug(f"Маскированный номер счёта: {masked}")
+    return masked
